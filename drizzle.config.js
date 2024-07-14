91bf25558a0add1e9e9c8700e39c39c33f011308
OM ASATI <asatiom1513@gmail.com>
2024-07-15 01:56:28 +0530

    db_url

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://ai-mock_owner:zF2wRBgdaAj6@ep-crimson-shape-a5grn02t.us-east-2.aws.neon.tech/ai-mock?sslmode=require',
+        url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
 };
